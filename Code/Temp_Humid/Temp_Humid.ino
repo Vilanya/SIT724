165301c33dbc8633f49cdf477b8b1eb4ad61aa1c
Vilanya <rratnayakemudiy@deakin.edu.au>
2022-06-06 19:02:46 +1000

    Added csv

--- Code/Temp_Humid/Temp_Humid.ino
+++ Code/Temp_Humid/Temp_Humid.ino
@@ -1,8 +1,8 @@
 int tempPin = 1;
-int humidpin = 0;
+int humidPin = 0;
 
-int val;
-int val2;
+int temp = 0;
+int humid = 0;
 
 float prev_temp = 0.0;
 float prev_humid = 0.0;
@@ -10,29 +10,29 @@ int t = 0;
 
 void setup()
 {
-  pinMode(sensorPin,INPUT);
-  pinMode(indicator,OUTPUT);
+pinMode(tempPin,INPUT);
+pinMode(humidPin,INPUT);
+//  pinMode(indicator,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
   //read sensor data
-  val = analogRead(tempPin);
-  val2 = analogRead(humidpin);   
+  temp = analogRead(tempPin);
+  humid = analogRead(humidPin);   
 
-  float mv = ( val/1024.0)*5000;
+  float mv = ( temp/1024.0)*5000;
   float temp_cel = mv/10;
 
 //  BetterSerialPlotter output format
- t=t+1;
+
  Serial.print(t);
  Serial.print(" ");
  Serial.print(temp_cel);
  Serial.print(" "); 
- Serial.println(val2);
-       
- 
-  delay(1000);
+ Serial.println(humid);
+ delay(1000);
+ t=t+1;
  
 }
